fdec0e1b6eb391519b7b38fbff49d2664598a634
Uroma002 <146116449+uroma002@users.noreply.github.com>
2023-11-03 17:58:27 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+
+    return data
+def main():
+    data = generate_random_data()
+def generate_random_data():
+    for item in data:
+
+        print(f"Random Number: {item}")
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    main()
